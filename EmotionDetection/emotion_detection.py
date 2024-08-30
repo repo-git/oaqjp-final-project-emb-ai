822d75d00987a928ab549d9a01eab4565688aa79
repo-git <unalias.na@gmail.com>
2024-08-30 14:09:30 -0400

    Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,6 +11,8 @@ def emotion_detector(text_to_analyze):
         Output
            string
     '''
+    # if text_to_analyze.strip():
+
     # URL of the Emotion Predict service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
@@ -23,15 +25,24 @@ def emotion_detector(text_to_analyze):
     # Sending a POST request to the API
     response = requests.post(url, json=myobj, headers=header)
 
-    # Parsing the JSON response from the API
-    formatted_response = json.loads(response.text)
-    
-    # Extract "emotions"
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    emotions = {}
+    if response.status_code == 200:
+        # Parsing the JSON response from the API
+        formatted_response = json.loads(response.text)
+        
+        # Extract "emotions"
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
 
-    # Evaluate max emotion
-    max_emotion = max(emotions, key=emotions.get)
-    emotions['dominant_emotion'] = max_emotion
+        # Evaluate max emotion
+        max_emotion = max(emotions, key=emotions.get)
+        emotions['dominant_emotion'] = max_emotion
+    elif response.status_code == 400:
+        emotions['anger'] = None
+        emotions['disgust'] = None
+        emotions['fear'] = None
+        emotions['joy'] = None
+        emotions['sadness'] = None
+        emotions['dominant_emotion'] = None
 
     # Returning a dictionary containing the response in text format
     return emotions
